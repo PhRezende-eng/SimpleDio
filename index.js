74f2073097e647db395f113e32419b12ef9e5491
PhRezende-eng <ph.hr.001@gmail.com>
2021-04-08 20:22:35 -0300

    #BeginProject Simple API Rest in Node Done! With filter!

--- index.js
+++ index.js
@@ -6,26 +6,44 @@ const SECRET = 'oKJhJsAHyBjAJHbm8#129%@';
 const contatos = require('./contatos.json');
 
 app.use(express.json());
+app.use(checkPermission);
+
 app.listen(port , () => {
     console.log('Servidor iniciado!');
 });
 
 app.post('/login', (req, res) => {
-    if(req.body.username === 'paulohenrique' && req.body.username === 'herique'){
+    if(req.body.username === 'paulohenrique' && req.body.password === 'henrique'){
         const id = 1;
-        const token = jwt.sign({id} , SECRET , {expiresIn: 300});
+        const token = jwt.sign({id} , SECRET );
         res.send({token});
     }else{
         res.status(403).send({message : 'Acesso Negado'});
     }
 })
 
+function checkPermission( req, res, next){
+    if ( req.path !== '/login' ) {
+        const token = req.headers['authorization'];
+        if(!token) return res.status(401).send({ message : 'Token não informado'})
 
+        jwt.verify(token, SECRET, function(err, decoded){
+            if(err) return res.status(500).send({ message : 'Acesso negado!'})
+            req.userId = decoded.id;
+            next();
+        })
+        
+    } else {
+        next();
+    }
+}
 
+app.get('/contatos/filtrar' , (req, res) => {
+    const queryNome = req.query.nome;
+    const contatosFiltrados = contatos.filter(c => c.nome_completo.toLowerCase().includes(queryNome.toLocaleLowerCase()));
+    res.send(contatosFiltrados);
+})
 
-
-
-
-app.get('/', (req, res) => {
+app.get('/contatos', (req, res) => {
     res.send(contatos)
 })
